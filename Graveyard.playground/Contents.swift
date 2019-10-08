534e90c13a92e97bac548eee0c6b8e416dce63ad
Joseph Rogers <joerogersss2016@gmail.com>
2019-10-08 05:29:29 -0500

    Closures before sleep

---
+++ Graveyard.playground/Contents.swift
@@ -0,0 +1,68 @@
+import Foundation
+//MARK: Closures
+
+        //simple closure
+let driving1 = {
+    print("I'm driving to my car")
+}
+
+driving1()
+
+        // MARK: parameters within a closure
+let driving2 = { (place: String) in
+    print("I am going to \(place) in my car")
+
+}
+
+driving2("London")
+
+    //more examples:
+
+var click = { (button: Int) in
+    if button >= 0 {
+        print("Button \(button) was clicked")
+    } else {
+        print("That button doesn't exist")
+    }
+}
+
+click(9)
+
+var sendMessage = { (message: String) in
+    if message != "" {
+        print("Sending to Twitter: \(message)")
+    }else{
+        print("Your message was empty")
+    }
+}
+sendMessage("Welcome to Twitter")
+
+let rowBoat = { (distance: Int) in
+    for _ in 1...distance {
+        print("I'm rowing 1 mile")
+    }
+}
+rowBoat(5)
+
+//MARK: Returning values from a closure
+//you can actually return value instead of just printing out information. do this by adding -> and a return type afterwards. right before your "in" and after your first bracket.
+
+let drivingWithReturn = { (place: String) -> String in
+    return "I am going to \(place) in my car."
+}
+
+let message = drivingWithReturn("St. Louis")
+print(message)
+
+//more examples:
+
+var hasGuitar = { (hasGuitar: Bool) -> Bool in
+    if hasGuitar {
+        print("Hey! I noticed you have a guitar! You wanna jam?")
+        return true
+    }else {
+        print("Man you need some new gear")
+        return false
+    }
+}
+hasGuitar(true)
