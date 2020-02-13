c253917201c8f27a43632b299d923ec710023157
EzraBlack <dev.ezrablack@gmail.com>
2020-02-13 12:45:07 -0600

    updates

--- Graveyard.playground/Pages/Date Formatting.xcplaygroundpage/Contents.swift
+++ Graveyard.playground/Pages/Date Formatting.xcplaygroundpage/Contents.swift
@@ -2,3 +2,15 @@
 //       df.dateStyle = .short
 //       df.timeStyle = .short
 //       timeSeenLabel.text = df.string(from: animal.timeSeen)
+
+//let decoder = JSONDecoder() // can throw an error so throw it in a do block
+//           decoder.dateDecodingStrategy = .secondsSince1970
+//           do {
+//               let animal = try decoder.decode(Animal.self, from: data)
+//               completion(.success(animal))
+//           } catch {
+//               NSLog("Error decoding animal object: \(error)")
+//               completion(.failure(.decodeError))
+//               return
+//           }
+//       }.resume()
