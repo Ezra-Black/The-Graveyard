e2f70e6b065b3b3e6950d6bd66163c55f6f88d9b
Ezra Black <dev.ezrablack@gmail.com>
2020-03-04 20:19:19 +0100

    Networking Boilerpl8

--- Graveyard.playground/Pages/networking boilerpl8.xcplaygroundpage/Contents.swift
+++ Graveyard.playground/Pages/networking boilerpl8.xcplaygroundpage/Contents.swift
@@ -1,7 +1,12 @@
-//: [Previous](@previous)
-
-import Foundation
-
-var str = "Hello, playground"
-
-//: [Next](@next)
+//enum HTTPMethod: String {
+//    case get = "GET"
+//    case post = "POST"
+//}
+//
+//enum NetworkError: Error {
+//    case noToken
+//    case badToken
+//    case unknownNetworkError
+//    case dataError
+//    case decodeError
+//}
