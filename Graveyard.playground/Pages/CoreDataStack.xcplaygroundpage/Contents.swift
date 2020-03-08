01d747f3edfe3e665a668947bf3df44ccb3ee12f
Ezra Black <dev.ezrablack@gmail.com>
2020-03-07 21:27:04 -0600

    CoreDataStack

--- Graveyard.playground/Pages/CoreDataStack.xcplaygroundpage/Contents.swift
+++ Graveyard.playground/Pages/CoreDataStack.xcplaygroundpage/Contents.swift
@@ -1,7 +1,34 @@
-//: [Previous](@previous)
-
-import Foundation
-
-var str = "Hello, playground"
-
-//: [Next](@next)
+//
+//class CoreDataStack {
+//
+//    static let shared = CoreDataStack()
+//
+//    private init() {}
+//
+//    lazy var container: NSPersistentContainer = {
+//        let container = NSPersistentContainer(name: "Users")
+//        container.loadPersistentStores { _, error in
+//            if let error = error {
+//                fatalError("Error loading from persistent stores: \(error)")
+//            }
+//        }
+//
+//        container.viewContext.automaticallyMergesChangesFromParent = true
+//        return container
+//    }()
+//
+//    var mainContext: NSManagedObjectContext {
+//        return container.viewContext
+//    }
+//    
+//    func save(context: NSManagedObjectContext = CoreDataStack.shared.mainContext) {
+//        context.performAndWait {
+//            do {
+//                try context.save()
+//            } catch {
+//                NSLog("Error saving to persistent stores: \(error)")
+//                context.reset()
+//            }
+//        }
+//    }
+//}
