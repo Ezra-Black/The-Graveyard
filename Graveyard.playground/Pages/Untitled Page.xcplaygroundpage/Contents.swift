34339f3cbe8e0840da6c1c6b1d7847642f749780
Ezra Black <dev.ezrablack@gmail.com>
2020-03-09 12:21:24 -0500

    set up main file so far from class

---
+++ Graveyard.playground/Pages/Untitled Page.xcplaygroundpage/Contents.swift
@@ -0,0 +1,59 @@
+import UIKit
+
+struct Person: Codable {
+    
+    enum PersonKeys: String, CodingKey {
+        case name
+        case height
+        case hairColor = "hair_color"
+        case created
+        case films
+    }
+    
+    let name: String
+    let height: Int // cm
+    let hairColor: String
+    let created: Date
+    let filmURLs: [URL]
+    
+    
+    //custom decoder
+    init(from decoder: Decoder) throws {
+        //container == Dictionary in JSON
+        //unKyedContainer == Array
+        let container = try decoder.container(keyedBy: PersonKeys.self)
+        
+        name = try container.decode(String.self, forKey: .name) //PersonKeys.name
+        hairColor = try container.decode(String.self, forKey: .hairColor)
+        
+        do {
+          height = try container.decode(Int.self, forKey: .height)
+        } catch {
+            let heightString = try container.decode(String.self, forKey: .height)
+            height = Int(heightString) ?? 0
+        }
+        
+        created = try container.decode(Date.self, forKey: .created)
+        //decoding nested json array of properties
+        filmURLs = try container.decode([URL].self, forKey: .films)
+    }
+}
+
+let url = URL(string: "https://swapi.co/api/people/1/")!
+let data = try! Data(contentsOf: url)
+
+//print(String(data: data, encoding: .utf8)!)
+
+let decoder = JSONDecoder()
+//decoder.keyDecodingStrategy = .convertFromSnakeCase
+
+
+//Date Formatter
+var dateFormatter = DateFormatter()
+dateFormatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'"
+decoder.dateDecodingStrategy = .formatted(dateFormatter)
+
+
+let luke = try! decoder.decode(Person.self, from: data)
+
+print(luke)
