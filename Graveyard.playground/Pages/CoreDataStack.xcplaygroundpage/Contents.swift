b4de0eadd0a93fcf4dcfb73953cbd781cc6d8a92
Ezra Black <dev.ezrablack@gmail.com>
2020-05-12 23:18:59 -0500

    updated coredatastack to include import of core data to stop missing it in production

--- Graveyard.playground/Pages/CoreDataStack.xcplaygroundpage/Contents.swift
+++ Graveyard.playground/Pages/CoreDataStack.xcplaygroundpage/Contents.swift
@@ -1,34 +1,20 @@
-//
-//class CoreDataStack {
-//
-//    static let shared = CoreDataStack()
-//
-//    private init() {}
-//
-//    lazy var container: NSPersistentContainer = {
-//        let container = NSPersistentContainer(name: "Users")
-//        container.loadPersistentStores { _, error in
-//            if let error = error {
-//                fatalError("Error loading from persistent stores: \(error)")
-//            }
-//        }
-//
-//        container.viewContext.automaticallyMergesChangesFromParent = true
-//        return container
-//    }()
-//
-//    var mainContext: NSManagedObjectContext {
-//        return container.viewContext
-//    }
-//    
-//    func save(context: NSManagedObjectContext = CoreDataStack.shared.mainContext) {
-//        context.performAndWait {
-//            do {
-//                try context.save()
-//            } catch {
-//                NSLog("Error saving to persistent stores: \(error)")
-//                context.reset()
-//            }
-//        }
-//    }
-//}
+import CoreData
+
+class CoreDataStack {
+    
+    static let shared = CoreDataStack()
+    
+    let container: NSPersistentContainer = {
+        
+        let container = NSPersistentContainer(name: "JournalCoreData" as String)
+        container.loadPersistentStores() { (storeDescription, error) in
+            if let error = error as NSError? {
+                fatalError("Unresolved error \(error), \(error.userInfo)")
+            }
+        }
+        return container
+    }()
+    
+    var mainContext: NSManagedObjectContext { return container.viewContext }
+}
+
