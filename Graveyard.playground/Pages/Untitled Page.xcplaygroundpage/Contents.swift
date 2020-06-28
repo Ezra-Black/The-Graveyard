01a2242352aa8d9f4665e5a8fbc523803f0fa1c8
Ezra Black <dev.ezrablack@gmail.com>
2020-06-27 22:19:58 -0500

    Added MockDataLoader BoilerPlate

---
+++ Graveyard.playground/Pages/Untitled Page.xcplaygroundpage/Contents.swift
@@ -0,0 +1,21 @@
+import Foundation
+//@testable import Oxygen
+//does a urlsession without a actual session.
+//no wifi? no network? no problem.
+class MockDataLoader: NetworkDataLoader {
+//
+    let data: Data?
+    let response: URLResponse?
+    let error: Error?
+
+    internal init(data: Data?, response: URLResponse?, error: Error?) {
+        self.data = data
+        self.response = response
+        self.error = error
+    }
+    func loadData(using request: URLRequest, completion: @escaping (Data?, URLResponse?, Error?) -> Void) {
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.005) {
+            completion(self.data, self.response, self.error)
+        }
+    }
+}
